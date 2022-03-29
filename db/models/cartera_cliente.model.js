cea583007bbf5c4ab61daa760efd552c0d9e3747
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-03-29 13:10:15 -0500

    Avance de models, faltan 7 tablas

---
+++ db/models/cartera_cliente.model.js
@@ -0,0 +1,57 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { USUARIO_TABLE } = require('./usuario.model');
+const { CLIENTE_TABLE } = require('./cliente.model');
+
+const CARTERA_CLIENTE_TABLE = 'cartera_cliente';
+
+const CarteraClienteSchema = {
+  codUsuario: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: USUARIO_TABLE,
+      key: 'cod_usuario'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+    field:'cod_usuario'
+  },
+  codCliente: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: CLIENTE_TABLE,
+      key: 'cod_cliente'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+    field:'cod_cliente'
+  },
+  fechaAsignacion: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field:'fecha_asignacion'
+  },
+  estado: {
+    allowNull: false,
+    type: DataTypes.CHAR(1)
+  }
+}
+
+class CarteraCliente extends Model {
+
+  static associate(models) {
+
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CARTERA_CLIENTE_TABLE,
+      modelName: 'CarteraCliente',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { CarteraCliente, CarteraClienteSchema, CARTERA_CLIENTE_TABLE }
