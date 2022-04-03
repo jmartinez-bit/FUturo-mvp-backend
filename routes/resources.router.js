2557dfaa74c2ec296526c58e00fb25412e3507f1
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-04-03 11:36:21 -0500

    Se aniadio la condicion de buscar periodo a partie del 2021, buscar clientes tambien  por periodo,buscar los colaboradores tambien por periodo

--- routes/resources.router.js
+++ routes/resources.router.js
@@ -11,19 +11,9 @@ router.post("/maparecursos",async (req, res,next) =>{
     const periodo=req.body.periodo;
     const cod_perfil=req.body.cod_perfil||null;
     const cod_colab=req.body.cod_colaborador||null;
-    if(cod_perfil===null && cod_colab===null){
-      const resources=await service.findByClientAndPeriod(cod_cliente,periodo);
-      res.json(resources);
-    }else if(cod_perfil===null && cod_colab!=null){
-      const resources=await service.findByClientPeriodAndNames(cod_cliente,periodo,cod_colab);
-      res.json(resources);
-    }else if(cod_perfil!=null && cod_colab===null){
-      const resources=await service.findByClientPeriodAndProfile(cod_cliente,periodo,cod_perfil);
-      res.json(resources);
-    }else if(cod_perfil!=null && cod_colab!=null){
-      const resources=await service.findByClientPeriodProfileAndNames(cod_cliente,periodo,cod_perfil,cod_colab);
-      res.json(resources);
-    }
+
+    const resources=await service.findByClientPeriodProfileAndNames(cod_cliente,periodo,cod_perfil,cod_colab);
+    res.json(resources);
 
   }catch (e){
     next(e);
@@ -43,10 +33,10 @@ router.get("/periodos",async (req, res,next) =>{
 });
 
 //Retorna todos los clientes de un Delivery Manager
-router.get("/:idDM/clientes",async (req, res,next) =>{
+router.get("/:idDM/clientes/:periodo",async (req, res,next) =>{
   try{
-    const {idDM}=req.params;
-    const customers=await service.findCustomers(idDM);
+    const {idDM,periodo}=req.params;
+    const customers=await service.findCustomers(idDM,periodo);
     res.json(customers);
   }catch (e){
     next(e);
@@ -67,10 +57,10 @@ router.get("/perfiles",async (req, res,next) =>{
 });
 
 //Retorna todos los nombres de los colaboradores
-router.get("/:id_cliente/colaboradores",async (req, res,next) =>{
+router.get("/:id_cliente/colaboradores/:periodo",async (req, res,next) =>{
   try{
-    const {id_cliente}=req.params;
-    const profiles=await service.findCollaboratorNames(id_cliente);
+    const {id_cliente,periodo}=req.params;
+    const profiles=await service.findCollaboratorNames(id_cliente,periodo);
     res.json(profiles);
   }catch (e){
     next(e);
--- services/resources.service.js
+++ services/resources.service.js
@@ -2,9 +2,6 @@
 //const { object } = require('joi');
 const sequelize=require('./../libs/sequelize')
 
-const select="SELECT linea_negocio,mapa_recursos.estado,perfil.nombre_perfil,mapa_recursos.nivel,fecha_inicio,fecha_fin,asignacion,clm_efectivo,produccion,productividad,CONCAT(nombres,' ',apellido_pat,' ',apellido_mat) AS nombre_colaborador "+
-             "FROM mapa_recursos INNER JOIN colaborador ON mapa_recursos.cod_colaborador=colaborador.cod_colaborador "+
-             "INNER JOIN perfil ON mapa_recursos.perfil=perfil.cod_perfil ";
 
 class ResourcesService{
 
@@ -12,43 +9,43 @@ class ResourcesService{
     this.products=[]
   }
 
-  async findByClientAndPeriod(cod_cliente,periodo){
-      const query=select+
-                  "WHERE cod_cliente="+cod_cliente+" AND periodo='"+periodo+"' ;";
-      const [data] = await sequelize.query(query);
-      return data ;
-  }
-
-  async findByClientPeriodAndProfile(cod_cliente,periodo,cod_perfil){
-    const query=select+
-                "WHERE cod_cliente="+cod_cliente+" AND periodo='"+periodo+"' AND mapa_recursos.perfil="+cod_perfil+" ;";
-    const [data] = await sequelize.query(query);
-    return data;
-  }
-
-  async findByClientPeriodAndNames(cod_cliente,periodo,cod_colab){
-    const query=select+
-                "WHERE cod_cliente="+cod_cliente+" AND periodo='"+periodo+"' AND mapa_recursos.cod_colaborador="+cod_colab+" ;";
-    const [data] = await sequelize.query(query);
-    return data;
-  }
-
   async findByClientPeriodProfileAndNames(cod_cliente,periodo,cod_perfil,cod_colab){
-    const query=select+
-                "WHERE cod_cliente="+cod_cliente+" AND periodo='"+periodo+"' AND mapa_recursos.perfil="+cod_perfil+" AND mapa_recursos.cod_colaborador="+cod_colab+" ;";
+    const select="SELECT linea_negocio,mapa_recursos.estado,perfil.nombre_perfil,mapa_recursos.nivel,"+
+              "fecha_inicio,fecha_fin,asignacion,clm_efectivo,produccion,productividad,CONCAT(nombres,' ',apellido_pat,' ',apellido_mat) AS nombre_colaborador "+
+             "FROM mapa_recursos "+
+             "INNER JOIN colaborador ON mapa_recursos.cod_colaborador=colaborador.cod_colaborador "+
+             "INNER JOIN perfil ON mapa_recursos.perfil=perfil.cod_perfil ";
+    var query=select+
+                "WHERE cod_cliente="+cod_cliente+" AND periodo='"+periodo+"' ";
+    if(cod_perfil!=null){
+      query+=" AND mapa_recursos.perfil="+cod_perfil;
+    }
+    if(cod_colab!=null){
+      query+=" AND mapa_recursos.cod_colaborador="+cod_colab;
+    }
+    query+=" ;";
     const [data] = await sequelize.query(query);
     return data;
   }
 
   async findPeriods(){
-    const query="SELECT periodo FROM periodo";
+    const query="SELECT periodo,estado FROM periodo "+
+                "WHERE CAST(SUBSTRING(periodo,1,4) AS int)>= 2021 ;";
     const [data] = await sequelize.query(query);
     return data;
   }
 
-  async findCustomers(idDM){
+  async findCustomers(idDM,periodo){
+    const split=periodo.split("-");
+    var year=parseInt(split[0],10);
+    var month=(parseInt(split[1],10)+1);
+    if(month===13){
+      month=1;
+      year+=1;
+    }
+    const date="01/"+month+"/"+year;
     const query="SELECT cartera_cliente.cod_cliente,nombre_corto FROM cliente INNER JOIN cartera_cliente ON cliente.cod_cliente=cartera_cliente.cod_cliente "+
-                "WHERE cartera_cliente.estado='A' AND cod_usuario="+idDM+";";
+                "WHERE cartera_cliente.estado='A' AND fecha_asignacion<= CAST('"+date+"' AS date) AND cod_usuario="+idDM+";";
     const [data] = await sequelize.query(query);
     return data;
   }
@@ -60,10 +57,10 @@ class ResourcesService{
     return data;
   }
 
-  async findCollaboratorNames(cod_cliente){
-    const query="SELECT cod_colaborador,nombres,apellido_pat,apellido_mat FROM colaborador "+
-                "WHERE EXISTS(SELECT cod_colaborador FROM mapa_recursos WHERE colaborador.cod_colaborador=mapa_recursos.cod_colaborador "+
-                "AND mapa_recursos.cod_cliente="+cod_cliente+")";
+  async findCollaboratorNames(cod_cliente,periodo){
+    const query="SELECT colaborador.cod_colaborador,nombres,apellido_pat,apellido_mat FROM colaborador "+
+                "INNER JOIN mapa_recursos ON colaborador.cod_colaborador=mapa_recursos.cod_colaborador "+
+                "WHERE mapa_recursos.cod_cliente="+cod_cliente+" AND periodo='"+periodo+"' ;";
     const [data] = await sequelize.query(query);
     return data;
   }
