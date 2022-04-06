5bf001635b2fd25891881d4b1918d2c856a349b8
Lincoln Martinez <lincoljosue7@gmail.com>
2022-04-05 20:42:15 -0500

    update table

--- services/assignments.service.js
+++ services/assignments.service.js
@@ -2,22 +2,22 @@ const sequelize = require('../libs/sequelize');
 
 // Sentencias
 function getSelect(attributes = '*') {
-  return `SELECT ${ attributes.toString() } FROM asignacion_recurso`;
+  return `SELECT ${ attributes.toString() } FROM asignacion_recursos`;
 };
-const joinService = `INNER JOIN servicio ON servicio.cod_servicio = asignacion_recurso.cod_servicio`;
+const joinService = `INNER JOIN servicio ON servicio.cod_servicio = asignacion_recursos.cod_servicio`;
 
 class AssignmentsService{
 
   async findByCodColaboradorJoinServicio(codColaborador, fechaIni, fechaFin) {
     // Columnas
     const select = getSelect(['servicio.tipo_servicio', 'servicio.descripcion_servicio',
-    'asignacion_recurso.por_asignacion', 'asignacion_recurso.fecha_inicio',
-    'asignacion_recurso.fecha_fin']);
+    'asignacion_recursos.por_asignacion', 'asignacion_recursos.fecha_inicio',
+    'asignacion_recursos.fecha_fin']);
 
     const query=`${ select } ${ joinService }
-                WHERE asignacion_recurso.cod_colaborador=${ codColaborador }
-                AND asignacion_recurso.fecha_inicio >= '${ fechaIni.toString() }'
-                AND asignacion_recurso.fecha_fin <= '${ fechaFin.toString() }';`;
+                WHERE asignacion_recursos.cod_colaborador=${ codColaborador }
+                AND asignacion_recursos.fecha_inicio >= '${ fechaIni.toString() }'
+                AND asignacion_recursos.fecha_fin <= '${ fechaFin.toString() }';`;
     const [data] = await sequelize.query(query);
     return data;
   }
