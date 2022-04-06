627cb9bd8ca640232deec1111acd0fb7dcf793c4
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-04-06 17:13:35 -0500

    Agregué felxibilidad al puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routerApi = require("./routes");
 const cors=require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
