b8e4046f6869e3fa052be6dc8df98871cab04577
Lincoln Martinez <lincoljosue7@gmail.com>
2022-04-18 18:00:04 -0500

    validar campos requeridos

--- routes/service.router.js
+++ routes/service.router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const ServicesService = require('./../services/services.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createServiceSchema } = require('./../schemas/service.schema');
+
 
 const router = express.Router();
 const service = new ServicesService();
 
 // Crear un servicio
-router.post('/create', async (req, res, next) => {
+router.post('/create',
+  validatorHandler(createServiceSchema, 'body'),
+  async (req, res, next) => {
     try {
       const body = req.body;
       res.status(201).json(await service.create(body));
---
+++ schemas/service.schema.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+
+const cod_cliente = Joi.number().integer();
+const cod_linea_servicio = Joi.number().integer();
+const tipo_servicio = Joi.string();
+const descripcion_servicio = Joi.string();
+const horas_venta = Joi.number().integer();
+const moneda = Joi.string();
+const valor_venta = Joi.number().integer();
+const fecha_ini_planificada = Joi.date();
+const fecha_fin_planificada = Joi.date();
+const fecha_ini_real = Joi.date();
+const fecha_fin_real = Joi.date();
+const forma_pago = Joi.string();
+const estado = Joi.string();
+
+const createServiceSchema = Joi.object({
+  cod_cliente: cod_cliente.required(),
+  cod_linea_servicio: cod_linea_servicio.required(),
+  tipo_servicio: tipo_servicio.required(),
+  descripcion_servicio: descripcion_servicio.required(),
+  horas_venta: horas_venta.required(),
+  moneda: moneda.required(),
+  valor_venta: valor_venta.required(),
+  fecha_ini_planificada: fecha_ini_planificada.required(),
+  fecha_fin_planificada: fecha_fin_planificada.required(),
+  fecha_ini_real: fecha_ini_real.allow(null),
+  fecha_fin_real: fecha_fin_real.allow(null),
+  forma_pago: forma_pago.required(),
+  estado: estado.required()
+});
+
+module.exports = { createServiceSchema }
