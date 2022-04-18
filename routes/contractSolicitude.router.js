7a878ac91edcffe7666314d8364d2f057810c98c
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-04-18 18:47:19 -0500

    Fix createsolicitud

--- routes/contractSolicitude.router.js
+++ routes/contractSolicitude.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const ContractSolicitudeService = require('../services/contractSolicitude.service');
 const ContractService = require('../services/contract.service');
 const SalaryBandService = require('../services/salaryBand.service');
+const EpsService = require('../services/eps.service');
+
 
 
 
@@ -9,7 +11,7 @@ const router = express.Router();
 const contractService = new ContractService();
 const contractSolicitudeService = new ContractSolicitudeService();
 const salaryBandService = new SalaryBandService();
-
+const epsService = new EpsService();
 
 router.post("/newSolicitude",async (req, res,next) =>{
   try{
@@ -19,10 +21,18 @@ router.post("/newSolicitude",async (req, res,next) =>{
        cod_banda_salarial, modalidad, remuneracion,fecha_inicio, fecha_fin}=req.body;
     //no obligatorios
       const bono_men=req.body.bono_men||null;
-      const ind_eps=req.body.ind_eps||null;
-      const monto_eps=req.body.ind_eps||null;
+      const cod_eps=req.body.cod_eps||null;
       const ind_sctr=req.body.ind_sctr||null;
       const condicional_adicional=req.body.condicional_adicional||null;
+    //hallar el monto del eps
+    console.log(cod_eps);
+
+    if(cod_eps!=null){
+    var monto_eps=parseFloat(await epsService.findAmount(cod_eps));
+    }else{
+        monto_eps=0;
+    }
+    console.log(monto_eps);
     //calculo del clm
     const asig_familiar=0;
     var clm=(remuneracion+asig_familiar)*1.47+monto_eps;
@@ -36,13 +46,15 @@ router.post("/newSolicitude",async (req, res,next) =>{
     }else{
       estado="Pendiente Aprobación GG";
     }
+    //Se acondiciona el numero de decimales de "clm"
+    clm=clm.toFixed(2);
     //se verifica si existe un contrato vigente
     const existAContract=  await contractService.isThereAContractActive(nro_documento)
     if (existAContract.length===0){
 
     await contractSolicitudeService.createSolicitude(tipo_documento, nro_documento, nombre, ape_paterno,
        ape_materno, fecha_nacimiento, nro_celular, correo, direccion, distrito, cod_cliente, cod_linea_negocio, cod_puesto,
-        nivel, modalidad, remuneracion, bono_men,ind_eps, monto_eps, ind_sctr, fecha_inicio, fecha_fin, condicional_adicional,clm,estado);
+        nivel,cod_banda_salarial, modalidad, remuneracion, bono_men,cod_eps, ind_sctr, fecha_inicio, fecha_fin, condicional_adicional,clm,estado);
 
     res.status(201).json("Nueva solicitud de contratación creada");
     }else{
--- services/eps.service.js
+++ services/eps.service.js
@@ -8,6 +8,13 @@ class EpsService{
      return data;
   }
 
+  async findAmount(cod_eps){
+    const query=`SELECT monto_eps from eps
+                WHERE cod_eps=${cod_eps}; `;
+    const [data] = await sequelize.query(query);
+    return Object.values(data[0])[0];;
+
+  }
 }
 
 module.exports = EpsService;
