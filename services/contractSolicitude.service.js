d7e973175cd3c7bc9b20c6ec6d541e7e1a0199d4
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-04-20 16:25:17 -0500

    Se arreglo el calculo del clm por cada modalidad y se implemento la busqueda de solicitudes por campos

--- services/contractSolicitude.service.js
+++ services/contractSolicitude.service.js
@@ -18,15 +18,21 @@ class ContractSolicitudeService{
             monto_eps=0;
         }
       //calculo del clm
-      var clm=(remuneracion)*process.env.FACTOR_SUELDO_Y_ASIG_FAMILIAR+monto_eps;
+      var mod=modalidad.toLowerCase();
+      var clm;
+      if(mod==="planilla"){
+        clm=(remuneracion)*process.env.FACTOR_PLANILLA+monto_eps;
+      } else if(mod==="rxh"||mod==="practicas"){
+        clm=remuneracion*process.env.FACTOR_RXH_PRACTICAS;
+      }
       if(bono_men){
         clm+=parseFloat(bono_men);
       }
       if(ind_sctr==='S'){
         clm+=remuneracion*process.env.PORCENTAJE_SCTR;
-      }
+        }
       //Se encuentra el cod_banda_salarial
-       const [codBanda]=await salaryBandService.findSalaryBand(nivel,cod_puesto);
+      const [codBanda]=await salaryBandService.findSalaryBand(nivel,cod_puesto);
       const cod_banda_salarial=codBanda.cod_banda_salarial;
       //verificar si el clm está dentro de la banda salarial
       const maximo=await salaryBandService.findMax(cod_banda_salarial);
@@ -61,6 +67,32 @@ class ContractSolicitudeService{
                 WHERE EXISTS (SELECT 1
                               FROM solicitud_contratacion
                               WHERE nro_documento = '${nroDocumento}'); `;
+    const [data] = await sequelize.query(query);
+     return data;
+  }
+
+  async findBy(body){
+    var query="SELECT * FROM solicitud_contratacion ";
+    if(body.length!=0){
+      query+="WHERE cod_solicitud_contratacion>0 ";
+    }
+    if(body.cod_cliente){
+      query+=`AND cod_cliente = '${body.cod_cliente}' `;
+    }
+    if(body.cod_linea_negocio){
+      query+=`AND cod_linea_negocio = '${body.cod_linea_negocio}' `;
+    }
+    if(body.nro_documento){
+      query+=`AND nro_documento = '${body.nro_documento}' `;
+    }
+    if(body.nombre){
+      const nombre=(body.nombre).toLowerCase();
+      query+=`AND (lower(CONCAT(nombre,' ',ape_paterno,' ',ape_materno)) like '%${nombre}%') `
+    }
+    if(body.estado){
+      query+=`AND estado = '${body.estado}'`;
+    }
+    query+=`;`;
     const [data] = await sequelize.query(query);
      return data;
   }
