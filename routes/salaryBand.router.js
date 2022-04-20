9ff8310de537b964ceb0e98f460b3531b8334034
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-04-20 18:55:48 -0500

    Mandar mensaje de error si no hay banda salarial para ese nivel y puesto

--- routes/salaryBand.router.js
+++ routes/salaryBand.router.js
@@ -8,7 +8,12 @@ router.get("/:cod_puesto/:nivel",async (req, res,next) =>{
   try{
     const {cod_puesto,nivel}=req.params;
     const data=await salaryBandService.findSalaryBand(nivel,cod_puesto);
+    if(data.length===0){
+      res.status(409).json({"error":false,
+        "message":"No existe banda salarial para este nivel y puesto"})
+    }else{
     res.json(data);
+    }
 
   }catch (e){
     next(e);
