f0929e499529eda0fd59fe62dd2995908178c843
Lincoln Martinez <lincoljosue7@gmail.com>
2022-04-20 10:58:42 -0500

    añadir numero hito en el create

--- services/payment-services.service.js
+++ services/payment-services.service.js
@@ -14,17 +14,28 @@ class PaymentServicesService{
 
   async create(data) {
     // Columnas
-    const insert = getInsert(['cod_servicio', 'descripcion_hito', 'horas', 'monto',
+    const insert = getInsert(['cod_servicio', 'numero_hito', 'descripcion_hito', 'horas', 'monto',
                               'fecha_inicio', 'fecha_fin']);
 
-    const query = `${ insert } VALUES(?, ?, ?, ?, ?, ?) RETURNING *;`;
+    const query = `${ insert } VALUES(?, ?, ?, ?, ?, ?, ?) RETURNING *;`;
     const [[ newPayment ]] = await sequelize.query(query, {
       type: QueryTypes.INSERT,
-      replacements: [data.cod_servicio, data.descripcion_hito, data.horas, data.monto,
+      replacements: [data.cod_servicio, (await this.countHitos(data.cod_servicio) + 1).toString(),
+                     data.descripcion_hito, data.horas, data.monto,
                      data.fecha_inicio, data.fecha_fin]
     });
 
     return newPayment;
+  }
+
+  async countHitos(codServicio) {
+
+    const select = getSelect(['count(*)']);
+
+    const query = `${ select } WHERE cod_servicio = ${ codServicio };`;
+    const [[data]] = await sequelize.query(query);
+
+    return parseInt(data.count);
   }
 
 }
