79579f9a8f91bb7b7927e64b1da143359d4b555d
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-05-01 17:10:40 -0500

    Se agrega el campo cod_puesto como salida del servicio showAssignments

--- services/assignments.service.js
+++ services/assignments.service.js
@@ -262,7 +262,7 @@ async updateEndDateOnResourcesMap(codColab,codServ,codCliente,codLinServ,periodo
  }
 
  async showAssignments(codServ){
-  const query=`SELECT cod_asignacion,asignacion_recursos.cod_servicio,asignacion_recursos.cod_colaborador,puesto.puesto,colaborador.nivel,CONCAT(nombres,' ',apellido_pat,' ',apellido_mat) AS nombres_apellidos,
+  const query=`SELECT cod_asignacion,asignacion_recursos.cod_servicio,asignacion_recursos.cod_colaborador,puesto.cod_puesto,puesto.puesto,colaborador.nivel,CONCAT(nombres,' ',apellido_pat,' ',apellido_mat) AS nombres_apellidos,
                 fecha_inicio,fecha_fin,por_asignacion,horas_asignacion,prod_planificada
                 FROM asignacion_recursos
                 INNER JOIN colaborador ON asignacion_recursos.cod_colaborador=colaborador.cod_colaborador
