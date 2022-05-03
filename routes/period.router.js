fee39547206a47c04966b5c1bd76f297c58f5cea
Lincoln Martinez <lincoljosue7@gmail.com>
2022-05-02 21:53:14 -0500

    documentacion apis

--- routes/period.router.js
+++ routes/period.router.js
@@ -6,7 +6,78 @@ const PeriodService = require('./../services/period.service');
 const router = express.Router();
 const periodService = new PeriodService();
 
-// TODO: Documentar apis
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    LastPeriod:
+ *      type: object
+ *      properties:
+ *        periodo:
+ *          type: string
+ *          description: periodo mensual actual (mm-yyyy)
+ *        tasa_cambio:
+ *          type: string
+ *          description: tasa de cambio actual
+ *      required:
+ *        - periodo
+ *        - tasa_cambio
+ *      example:
+ *        periodo: 04-2022
+ *        tasa_cambio: 3.77
+ *    GetPeriods:
+ *      type: object
+ *      properties:
+ *        periodo:
+ *          type: string
+ *          description: periodo mensual actual (mm-yyyy)
+ *        tasa_cambio:
+ *          type: string
+ *          description: tasa de cambio actual
+ *        fecha_apertura:
+ *          type: date
+ *          description: fecha de registro del periodo
+ *        estado:
+ *          type: string
+ *          description: estado del periodo
+ *      required:
+ *        - periodo
+ *        - tasa_cambio
+ *        - fecha_apertura
+ *        - estado
+ *      example:
+ *        periodo: 04-2022
+ *        tasa_cambio: 3.77
+ *        fecha_apertura: 15-04-2022
+ *        estado: A
+ *    UpdatePeriod:
+ *      type: object
+ *      properties:
+ *        tasa_cambio:
+ *          type: string
+ *          description: tasa de cambio actual
+ *      required:
+ *        - tasa_cambio
+ *      example:
+ *        tasa_cambio: 3.77
+ */
+
+// Buscar ultimo periodo.
+/**
+ * @swagger
+ * /api/v1/period/last-period:
+ *  get:
+ *    summary: Buscar tipos de servicio por linea de servicio
+ *    tags: [Periodo]
+ *    responses:
+ *      200:
+ *        description: obtener ultimo periodo con tasa de cambio
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/LastPeriod'
+ */
 router.get('/last-period', async (req, res, next) => {
   try {
     const lastPeriod = await periodService.getLastPeriod();
@@ -16,6 +87,31 @@ router.get('/last-period', async (req, res, next) => {
   }
 });
 
+// Aperturar periodo.
+/**
+ * @swagger
+ * /api/v1/period/create:
+ *  post:
+ *    summary: registrar un nuevo periodo
+ *    tags: [Periodo]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/LastPeriod'
+ *    responses:
+ *      201:
+ *        description: nuevo periodo creado
+ *      409:
+ *        description: hubo conflictos al momento de crear
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/Unauthorized'
+ */
 router.post('/create',
   validatorHandler(createPeriodSchema, 'body'),
   async (req, res, next) => {
@@ -27,6 +123,24 @@ router.post('/create',
   }
 });
 
+// Actualizar ultimo periodo.
+/**
+ * @swagger
+ * /api/v1/period/update:
+ *  put:
+ *    summary: actualizar ultimo periodo
+ *    tags: [Periodo]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/UpdatePeriod'
+ *    responses:
+ *      200:
+ *        description: periodo actualizado
+ */
 router.put('/update',
   validatorHandler(updatePeriodSchema, 'body'),
   async (req, res, next) => {
@@ -38,6 +152,22 @@ router.put('/update',
   }
 });
 
+// Obtener los ultimos 6 periodos.
+/**
+ * @swagger
+ * /api/v1/period:
+ *  get:
+ *    summary: listar ultimos 6 periodos
+ *    tags: [Periodo]
+ *    responses:
+ *      200:
+ *        description: Listado de periodos
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/GetPeriods'
+ */
 router.get('/',
   async (req, res, next) => {
   try {
