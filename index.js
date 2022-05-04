ede120e1dd41303336e25879d331414c406db3d9
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-05-03 20:48:25 -0500

    Intento 1 arreglar problema de cors con la subida de archivos binarios

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ const swaggerSpec = {
 }
 
 const app = express();
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 const port = process.env.PORT ||3000;
 
 app.use(express.json());
