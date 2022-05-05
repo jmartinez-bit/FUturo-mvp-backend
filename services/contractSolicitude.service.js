d1b3d9a43f379ba888a1d792fa9c60f7d659b633
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-05-05 12:49:23 -0500

    Se amplio los campos que se muestran en findOne

--- services/contractSolicitude.service.js
+++ services/contractSolicitude.service.js
@@ -109,7 +109,8 @@ class ContractSolicitudeService{
   async findOne(cod){
     const query1=`SELECT cod_solicitud_contratacion,tipo_documento,nro_documento,nombre,ape_paterno,ape_materno,fecha_nacimiento,
               nro_celular,correo,direccion,distrito,provincia,nombre_corto,cod_linea_negocio,solicitud_contratacion.cod_puesto,
-              puesto,nivel,cod_banda_salarial,modalidad,remuneracion,bono_men,ind_asign_familiar,fecha_inicio,fecha_fin,condicional_adicional,solicitud_contratacion.estado
+              puesto,nivel,cod_banda_salarial,modalidad,remuneracion,bono_men,ind_asign_familiar,fecha_inicio,fecha_fin,condicional_adicional,solicitud_contratacion.estado,
+              condicion_proyecto_area,tarifa_mensual,jefe_responsable_directo,horario_laboral,asignacion_equipo,cv
                  FROM solicitud_contratacion
                  INNER JOIN cliente ON solicitud_contratacion.cod_cliente=cliente.cod_cliente
                  INNER JOIN puesto ON solicitud_contratacion.cod_puesto=puesto.cod_puesto
