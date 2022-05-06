e1eb4e8fcdb77ae4afc4d78f2ec6268fdc13f248
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-05-06 15:37:54 -0500

    Se cambio el estado a buscar en client find all de 'activo' a 'A'

--- services/client.service.js
+++ services/client.service.js
@@ -21,7 +21,7 @@ class ClientService{
   }
 
   async findAll(){
-    const query=`SELECT cod_cliente,nombre_corto FROM cliente WHERE estado='activo' ORDER BY nombre_corto ASC;`;
+    const query=`SELECT cod_cliente,nombre_corto FROM cliente WHERE estado='A' ORDER BY nombre_corto ASC;`;
     const [data] = await sequelize.query(query);
      return data;
   }
