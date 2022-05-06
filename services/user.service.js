63ca4a65af45891f5539eb50976b125045d4292d
Carlos Miguel <canchayhuar@uni.pe>
2022-05-06 17:58:46 -0500

    Agregue datos al token

--- services/user.service.js
+++ services/user.service.js
@@ -9,7 +9,7 @@ class UserService{
   }
 
   async findByEmail(email) {
-    const query=`SELECT cod_usuario, usuario, password, a.cod_perfil, REPLACE(UPPER(b.nombre_perfil), ' ', '_') AS nombre_perfil FROM usuario a INNER JOIN perfil b ON a.cod_perfil = b.cod_perfil WHERE usuario='${email}'; `;
+    const query=`SELECT cod_usuario, usuario, password, a.cod_perfil, REPLACE(UPPER(b.nombre_perfil), ' ', '_') AS nombre_perfil, nombres_apellidos FROM usuario a INNER JOIN perfil b ON a.cod_perfil = b.cod_perfil WHERE usuario='${email}' and a.estado = 'A'; `;
     const [[rta]] = await sequelize.query(query);
     return rta;
   }
