fa4d547f670cb41b0cd120286ebfe09138c48147
Lincoln Martinez <lincoljosue7@gmail.com>
2022-05-18 09:09:17 -0500

    IFU-283 registro de solicitud de renovacion (mc)

--- routes/renovation-request.router.js
+++ routes/renovation-request.router.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const validatorHandler = require('../middlewares/validator.handler');
-const { checkRoles  }  = require('./../middlewares/auth.handler');
 const RenovationRequestService = require('./../services/renovation-request.service');
 
 const router = express.Router();
 const renovationRequestService = new RenovationRequestService();
 
-// Registrar solicitud de renovacion mismas condiciones o cambio contractual
 router.post('/create',
-  // validatorHandler(createPeriodSchema, 'body'),
   async (req, res, next) => {
   try {
     const body = req.body;
