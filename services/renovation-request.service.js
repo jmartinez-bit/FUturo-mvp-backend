c88de80b268dfd992220b2401b6398097ca33bb8
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-05-19 18:23:25 -0500

    Fix findBy from solicitudeService

--- services/renovation-request.service.js
+++ services/renovation-request.service.js
@@ -39,5 +39,35 @@ class RenovationRequestService {
     }
   }
 
+  findBy(body){
+    var query=`SELECT tipo_solicitud,cod_solicitud_contratacion,solicitud_contratacion.fecha_reg,cliente.nombre_corto,
+    cod_linea_negocio,puesto.puesto,nivel,nro_documento,CONCAT(nombre,' ',ape_paterno,' ',ape_materno) AS nombre_apellidos,
+    modalidad,remuneracion,bono_men,solicitud_contratacion.estado,fecha_aprob,ind_aprobacion_gg,fecha_aprob_gg
+    FROM solicitud_renovacion
+    INNER JOIN cliente ON solicitud_contratacion.cod_cliente=cliente.cod_cliente
+    INNER JOIN puesto ON solicitud_contratacion.cod_puesto=puesto.cod_puesto `;
+    if(body.length!=0){
+      query+="WHERE cod_solicitud_contratacion>0 ";
+    }
+    if(body.cod_cliente){
+      query+=`AND solicitud_contratacion.cod_cliente = '${body.cod_cliente}' `;
+    }
+    if(body.cod_linea_negocio){
+      query+=`AND cod_linea_negocio = '${body.cod_linea_negocio}' `;
+    }
+    if(body.nro_documento){
+      query+=`AND nro_documento = '${body.nro_documento}' `;
+    }
+    if(body.nombre){
+      const nombre=(body.nombre).toLowerCase();
+      query+=`AND (lower(CONCAT(nombre,' ',ape_paterno,' ',ape_materno)) like '%${nombre}%') `
+    }
+    if(body.estado){
+      query+=`AND solicitud_contratacion.estado = '${body.estado}'`;
+    }
+
+     return query;
+  }
+
 }
 module.exports = RenovationRequestService;
