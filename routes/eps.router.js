224c8a58b110281c27f10ddd13d75d5b9d4eb969
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-05-19 18:08:54 -0500

    Fix some code smells

--- routes/eps.router.js
+++ routes/eps.router.js
@@ -4,7 +4,7 @@ const EpsService = require('../services/eps.service');
 const router = express.Router();
 const epsService = new EpsService();
 
-router.get("/",async (req, res,next) =>{
+router.get("/",async (_req, res,next) =>{
   try{
     const data=await epsService.findAll();
     res.json(data);
